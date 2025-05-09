634c9623154554174d92c586d8ac177d9b295fb8
Mustafa-Sayed-M <mustafacoder9@gmal.com>
2025-05-09 14:05:48 +0300

    Update Download Cv

--- src/Components/Atoms/DownloadCv.jsx
+++ src/Components/Atoms/DownloadCv.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 function DownloadCv() {
     return (
         <a
-            target='_blank'
             rel="noreferrer"
             href={`https://raw.githubusercontent.com/mustafa-sayed-m/my-portfolio-backend/master/files/cv.pdf`}
             className='block text-center py-2 px-8 rounded-full border-2 border-purple-color text-purple-color font-medium transition sm:hover:bg-purple-color sm:hover:text-white'
